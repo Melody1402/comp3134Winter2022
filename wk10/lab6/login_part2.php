20dd990705e31aef5dde28f4c4be0d7aad32cdb1
Melody1402 <thithuy.nguyen@georgebrown.ca>
2022-04-12 13:58:31 -0400

    upload lab6

---
+++ wk10/lab6/login_part2.php
@@ -0,0 +1,22 @@
+<h1>Weak Password</h1> <form method="post">
+<label>Password</label>
+<input type="password" name="password"> <br/>
+<input type="submit">
+</form>
+<?php
+$pass = array(123456, 123456789, "qwerty","password", 111111, 12345678, "abc123", 1234567, " password1", 12345);
+for($var i=0;i<pass.length;i++){
+    if(password ==pass[i]{
+        alert("Sucessfully authenticated");
+    })
+};
+<form method = "post">
+<label>Guess username</label>
+<input type="text" name = "username">
+<br/>
+<input type="submit">
+
+</form>
+$name = "Thi"
+if(username == name){alert("Welcome ${name} to Your Portal");};
+?>
